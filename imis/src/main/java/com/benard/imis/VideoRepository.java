172ceaf88041104202386b52e9b5d844181365bf
benardop <benard.oc.p@gmail.com>
2023-08-31 19:00:07 -0300

    Add video repository class

--- imis/src/main/java/com/benard/imis/VideoRepository.java
+++ imis/src/main/java/com/benard/imis/VideoRepository.java
@@ -2,5 +2,8 @@ package com.benard.imis;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VideoRepository extends JpaRepository<VideoEntity, Long> {
+    List<VideoEntity> findByName(String name);
 }
